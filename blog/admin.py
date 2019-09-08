6e14926dd6f06773dcf70fff6d8324d93671046c
马庆 <maqing1013@gmai.com>
2019-09-08 15:41:06 +0800

    初始化提交

---
+++ blog/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Post, Category, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'created_time', 'modified_time']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
